ef3dd57713931288bc84510aa5c13b23aa3d66b1
vovitolog <vovitolog@gmail.com>
2022-07-18 21:20:39 +0300

    feat: добавить подключение к БД

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('teeee');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3001 } = process.env;
+const app = express();
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});
